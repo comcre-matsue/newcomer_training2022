7fbcc49b2b0cb8907c9ee05b00e1ec6135d5082d
hirashita <your.hirashita@comcre.co.jp>
2022-04-06 16:23:21 +0900

    4-1

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
   get 'static_pages/help'
   get  'static_pages/about'
   get  'static_pages/contact'
-  resources :microposts
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
 end
