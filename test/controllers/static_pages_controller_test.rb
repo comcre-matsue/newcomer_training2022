38c963f0de81f7421822ce2e208664a986aaa4f4
shimohashi_comc <shimohashi@comcre.co.jp>
2022-04-06 14:12:51 +0900

    finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -10,4 +10,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
   end
+  
+  test "should get help" do
+    get static_pages_help_url
+    assert_response :success
+  end
 end
