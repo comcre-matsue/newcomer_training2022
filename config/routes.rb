1d19240638c0bc688bf46d68b788a3261d2993b2
hayashibaracom <comcre.hayashibara@gmail.com>
2022-04-06 13:47:55 +0900

    3-1,3-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
+  root 'application#hello'
   resources :microposts
   resources :users
-  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
