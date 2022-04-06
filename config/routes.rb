e52ea5158183622b33ba1cee90105d75712e2368
shimohashi_comc <shimohashi@comcre.co.jp>
2022-04-06 15:09:40 +0900

    finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   resources :microposts
   resources :users
-  root "users#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
