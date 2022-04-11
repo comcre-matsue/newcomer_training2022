91fc87c976e0c6e6331bf318b8f8ccec4bcf459d
Asada_com <asada@comcre.co.jp>
2022-04-11 17:39:18 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  resources :microposts
-  resources :users
+  get 'users/new', to: 'users#new', as:'signup_path'
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get  'help',    to: 'static_pages#help' 
+  get  'about',   to: 'static_pages#about'
+  get  'contact', to: 'static_pages#contact'
+  get  'signup',  to: 'users#new'
 end
