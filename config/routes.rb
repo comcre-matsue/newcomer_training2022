b5f98c304722391ebc6a7870a471c7fe39a6d481
hirashita <your.hirashita@comcre.co.jp>
2022-04-12 16:48:47 +0900

    7-1

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
+  resources :users
 end
