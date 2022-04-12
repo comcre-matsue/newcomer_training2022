432daf0cfdd19d9d27075295571eb9a99fd5a0c1
Asada_com <asada@comcre.co.jp>
2022-04-12 17:36:26 +0900

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'bootstrap-sass', '3.3.7'
 
 gem 'rails-controller-testing'
 
+gem 'bcrypt',         '3.1.12'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_012847) do
+ActiveRecord::Schema.define(version: 2022_04_12_074535) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2022_04_06_012847) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
