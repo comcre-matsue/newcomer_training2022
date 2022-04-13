4629a004465ccf5d1cf29567651107aac839e049
hirashita <your.hirashita@comcre.co.jp>
2022-04-13 10:55:13 +0900

    7-4

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static_pages#home'        
   resources :users
