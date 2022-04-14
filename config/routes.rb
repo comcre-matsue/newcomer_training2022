6c1366c54d934620e3dfc171090c324547112fb8
hirashita <your.hirashita@comcre.co.jp>
2022-04-14 11:49:20 +0900

    9-4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get  '/home',    to: 'static_pages#home'
+  # get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
