f0930425a706d0427d23a78ace346220872fecf7
Asada_com <asada@comcre.co.jp>
2022-04-14 17:43:36 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,18 +3,24 @@ class SessionsController < ApplicationController
   end
   
   def create
-       user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = t('.login_error')
       render 'new'
     end
   end
 
   def destroy
     log_out
+    redirect_to root_url
+  end
+
+  def destroy
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,12 +7,15 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    # ssert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path
+
     get contact_path
     assert_select "title", full_title("Contact")
+    
     get signup_path
     assert_select "title", full_title("Signup")
   end
