f332672680388cf00798aea492c2bcc789b6c537
hirashita <your.hirashita@comcre.co.jp>
2022-04-15 16:41:43 +0900

    11

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -7,5 +7,5 @@ module UsersHelper
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
-  end
+  end 
 end
