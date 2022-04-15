b0f86faf2b5c32c66924d594636398a4e1d6dacd
hayashibaracom <comcre.hayashibara@gmail.com>
2022-04-15 14:15:52 +0900

    12-3

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,8 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: [:edit, :update]
+  before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
+
   def new
   end
 
@@ -17,4 +21,49 @@ class PasswordResetsController < ApplicationController
   
   def edit
   end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, :blank)
+      render 'edit'
+    elsif @user.update(user_params)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private 
+
+    def user_params
+      params.require(:user).permit(:password, :password_confirmation)
+    end
+    
+    #beforeフィルター
+
+    #ユーザーの取得
+    def get_user
+      @user = User.find_by(email: params[:email])
+    end
+
+    #有効なユーザーかどうか確認する
+    def valid_user
+      unless (@user && @user.activated? &&
+              @user.authenticated?(:reset, params[:id]))
+        redirect_to root_url
+      end
+    end
+
+    #トークンが期限切れか確認する
+    def check_expiration
+      if @user.password_reset_expired?
+        flash[:danger] = "Password reset has expired."
+        redirect_to new_password_reset_url
+      end
+    end
+    
+    
 end
