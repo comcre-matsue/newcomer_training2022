8d75e67c460792a3afbd890c8ab2ed0742119c94
hayashibaracom <comcre.hayashibara@gmail.com>
2022-04-15 16:09:39 +0900

    13-2

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,19 @@ User.create!(name:  "Example User",
   activated_at: Time.zone.now)
 
 99.times do |n|
-name  = Faker::Name.name
-email = "example-#{n+1}@railstutorial.org"
-password = "password"
-User.create!(name:  name,
-   email: email,
-   password:              password,
-   password_confirmation: password,
-   activated: true,
-   activated_at: Time.zone.now)
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name:  name,
+    email: email,
+    password:              password,
+    password_confirmation: password,
+    activated: true,
+    activated_at: Time.zone.now)
+end
+
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
