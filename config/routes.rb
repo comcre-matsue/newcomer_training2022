2f16aed342973f42a935017645d5a2f90e97517c
hirashita <your.hirashita@comcre.co.jp>
2022-04-18 10:13:57 +0900

    12

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   # get  '/home',    to: 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
@@ -12,5 +14,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'        
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets, only: [:new, :create, :edit, :update]
   
 end
