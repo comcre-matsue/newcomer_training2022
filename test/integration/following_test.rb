2b0798fadb90c1b3d2a3f8f3831210de1486a341
shimohashi_comc <shimohashi@comcre.co.jp>
2022-04-19 11:13:04 +0900

    fix icon

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -52,4 +52,11 @@ class FollowingTest < ActionDispatch::IntegrationTest
       delete relationship_path(relationship), xhr: true
     end
   end
+
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
 end
