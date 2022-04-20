5e9e5ee3bdc0b1136ea6eb1c568e56a579fcbfea
Asada_com <asada@comcre.co.jp>
2022-04-20 14:44:59 +0900

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_18_055621) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_011253) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -18,10 +18,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_18_055621) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at", precision: nil
-    t.boolean "admin", default: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
